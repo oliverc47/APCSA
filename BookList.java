24a930462341d5f0a4403d27248d8ea457fcea6a
oliverc47 <144722149+oliverc47@users.noreply.github.com>
2023-11-14 13:58:38 -0500

    Add files via upload

---
+++ BookList.java
@@ -0,0 +1,16 @@
+public class BookList
+{
+  private List<String> inventory = new ArrayList<String>();
+  
+  
+  public ArrayList<String> getInventory()
+  {
+    return inventory;
+  }
+  
+  
+  public void addBook(String b) 
+  {
+    iventory.add(b);
+  }
+}
